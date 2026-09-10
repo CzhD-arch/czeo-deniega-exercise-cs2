b6b9f8cf791c607636d7c7c6e758b8c106cb15e3
Czeo G. Deniega <252540087+CzhD-arch@users.noreply.github.com>
2026-09-10 11:28:08 +0800

    Update odd_or_even.py

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,9 +1,10 @@
 import math
 
+# Ask for number
 number = int(input("Enter your number:"))
-
+# Calculate the remainder
 remainder = number % 2
-
+# Select if the number is odd or even based on the remainder
 if remainder == 1:
     print(f"{number} is odd.")
 if remainder == 0:
